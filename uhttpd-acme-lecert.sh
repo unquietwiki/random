dbb0b0c2e5051b351e5841da885a3921c1aeb2a0
Michael Adams <unquietwiki@gmail.com>
2018-09-20 18:43:01 -0700

    Update uhttpd-acme-lecert.sh

--- uhttpd-acme-lecert.sh
+++ uhttpd-acme-lecert.sh
@@ -12,15 +12,12 @@
 DOMAIN=example.com
 
 echo Adjusting firewall rules to make sure HTTP and HTTPS are allowed externally
-iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A zone_wan_input -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A zone_wan_input -p tcp -m tcp --dport 443 -j ACCEPT
+ip6tables -A zone_wan_input -p tcp -m tcp --dport 80 -j ACCEPT
+ip6tables -A zone_wan_input -p tcp -m tcp --dport 443 -j ACCEPT
 
-echo Purging old ACME cert from uhttpd...
-killall -9 uhttpd
-rm /etc/ssl/private/uhttpd.key
-rm /etc/ssl/certs/uhttpd.crt
+echo Starting ACME/LE pull
 /etc/init.d/acme restart
 
 echo Waiting on new ACME cert...
@@ -30,6 +27,7 @@ echo Restoring original firewall...
 /etc/init.d/firewall restart
 
 echo Loading new ACME cert...
+killall -9 uhttpd
 openssl rsa -in /etc/acme/$DOMAIN/$DOMAIN.key -outform DER -out /etc/ssl/private/uhttpd.key
 openssl x509 -in /etc/acme/$DOMAIN/fullchain.cer -outform DER -out /etc/ssl/certs/uhttpd.crt
 /etc/init.d/uhttpd restart
